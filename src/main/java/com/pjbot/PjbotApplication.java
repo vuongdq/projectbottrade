5246f8bd0f78f96f7bb415daa457678435ad8e77
vuongdq <quocvuong_it@yahoo.com>
2022-07-20 17:19:44 +0700

    init project

---
+++ src/main/java/com/pjbot/PjbotApplication.java
@@ -0,0 +1,13 @@
+package com.pjbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pjbot/PjbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
